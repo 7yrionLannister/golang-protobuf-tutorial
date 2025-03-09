a11ce147c1e0122686e1a73deef6f6f098d7f798
Daniel Fernández <47828494+7yrionLannister@users.noreply.github.com>
2025-03-09 11:56:08 -0500

    chore: finish twirp tutorial

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/7yrionLannister/golang-protobuf-tutorial/generated/tutorialpb"
 	"google.golang.org/protobuf/proto"
+
+	pb "github.com/7yrionLannister/golang-protobuf-tutorial/generated2/twirptutorial"
 )
 
 const fname = "myproto.binary"
@@ -51,4 +56,51 @@ func main() {
 	}
 	fmt.Printf("Retrieved book from binary file: %+v\n", retrievedBook)
 	fmt.Printf("Original book: %+v\n", book)
+
+	twirpExample()
+}
+
+// --------------------------------------------------------------------------------------
+// twirp example: No entiendas el código autogenerado, concentrate en esto
+
+// Implementation of pb.HelloWorld
+type HelloWorldServer struct{}
+
+// Receives and handles the RPC request through HTTP
+func (s *HelloWorldServer) Hello(ctx context.Context, req *pb.HelloReq) (*pb.HelloResp, error) {
+	fmt.Println("Server received RPC request from client and It's processing it")
+	return &pb.HelloResp{Text: "Hello " + req.Subject}, nil
+}
+
+func twirpExample() {
+	// CLIENT
+	// Because we are creating a Protobuf client, the Content-Type of all requests is going to be "application/protobuf"
+	client := pb.NewHelloWorldProtobufClient("http://localhost:8080", &http.Client{})
+	timer := time.NewTimer(time.Second * 10)
+	ticker := time.NewTicker(time.Second * 2)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				// Sends RPC HTTP request using "application/protobuf" as Content Type
+				resp, err := client.Hello(context.Background(), &pb.HelloReq{Subject: "World"})
+				if err == nil {
+					fmt.Println("Client received response:", resp.Text) // prints "Hello World"
+				} else {
+					fmt.Println("Error:", err)
+				}
+			case <-timer.C:
+				return
+			}
+		}
+	}()
+
+	// SERVER
+	twirpHandler := pb.NewHelloWorldServer(&HelloWorldServer{})
+	// You can use any mux you like - NewHelloWorldServer gives you an http.Handler.
+	mux := http.NewServeMux()
+	// The generated code includes a method, PathPrefix(), which
+	// can be used to mount your service on a mux.
+	mux.Handle(twirpHandler.PathPrefix(), twirpHandler)
+	http.ListenAndServe(":8080", mux)
 }
